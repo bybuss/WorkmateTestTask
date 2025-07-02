6726c67440025c44d060cc5d51e5b3eef0097f08
bybuss <kukuhaha729@gmail.com>
2025-07-02 19:29:47 +0300

    deleted: unused methods

--- app/src/main/java/com/example/currencyconverter/common/utils/UiState.kt
+++ app/src/main/java/com/example/currencyconverter/common/utils/UiState.kt
@@ -5,15 +5,3 @@ sealed interface UiState<out T> {
     data class Success<T>(val data: T) : UiState<T>
     data class Error(val title: String, val text: String) : UiState<Nothing>
 }
-
-fun <T> UiState<T>.updateIfSuccess(block: T.() -> T): UiState<T> {
-    return when (this) {
-        is UiState.Success -> UiState.Success(data.block())
-        else -> this
-    }
-}
-
-fun <T> UiState<T>.takeIfSuccess(): T? = when (this) {
-    is UiState.Success -> data
-    else -> null
-}
