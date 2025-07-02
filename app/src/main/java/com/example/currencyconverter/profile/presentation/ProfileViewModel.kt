520becd617fe2249478ee077d71003b1fdd3e0eb
bybuss <kukuhaha729@gmail.com>
2025-07-02 23:43:47 +0300

    fixed: change type to flow

--- app/src/main/java/com/example/currencyconverter/profile/presentation/ProfileViewModel.kt
+++ app/src/main/java/com/example/currencyconverter/profile/presentation/ProfileViewModel.kt
@@ -5,16 +5,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.currencyconverter.profile.domain.use_cases.GetAccountsUseCase
 import com.example.currencyconverter.home.domain.use_cases.GetFlagResIdUseCase
+import com.example.currencyconverter.profile.domain.use_cases.GetAccountsFlowUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    private val getAccounts: GetAccountsUseCase,
-    private val getFlagResId: GetFlagResIdUseCase,
+    private val getAccountsFlow: GetAccountsFlowUseCase,
+    private val getFlagResId: GetFlagResIdUseCase
 ) : ViewModel() {
 
     var state by mutableStateOf(ProfileState())
@@ -26,9 +26,11 @@ class ProfileViewModel @Inject constructor(
 
     private fun load() {
         viewModelScope.launch {
-            val account = getAccounts().firstOrNull()
-            val flag = account?.let { getFlagResId(it.code) }
-            state = state.copy(account = account, flagRes = flag)
+            getAccountsFlow().collect { accounts ->
+                val account = accounts.firstOrNull()
+                val flag = account?.let { getFlagResId(it.code) }
+                state = state.copy(account = account, flagRes = flag)
+            }
         }
     }
 }
